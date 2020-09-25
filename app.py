1e94262b8ec034cfbee03e6014b97cb28babcaf3
rishabaggarwal247 <rishabaggarwal247@gmail.com>
2020-09-25 17:59:24 +0530

    api startup test3.1

--- app.py
+++ app.py
@@ -72,41 +72,49 @@ def token_required(f):
 
 @app.route('/verify', methods=['POST'])
 def verify():
+    try:
+        app.logger.info('test4')
+        # starting time
+        start = time.time()
 
-    # starting time
-    start = time.time()
+        app.logger.info('test5')
+        print(time.time() - start)
+        browser.get('https://parivahan.gov.in/rcdlstatus/vahan/rcDlHome.xhtml')
+        print(time.time() - start)
+        plateNumber = ""
+        captchaAnswer = ""
 
-    print(time.time() - start)
-    browser.get('https://parivahan.gov.in/rcdlstatus/vahan/rcDlHome.xhtml')
-    print(time.time() - start)
-    plateNumber = ""
-    captchaAnswer = ""
+        app.logger.info('test6')
+        # captcha selector tag : //*[@id="form_rcdl:j_idt32:j_idt37"]
 
-    # captcha selector tag : //*[@id="form_rcdl:j_idt32:j_idt37"]
+        browser.find_element_by_xpath('//*[@id="form_rcdl:tf_reg_no1"]').send_keys(plateNumber[:-4])
+        browser.find_element_by_xpath('//*[@id="form_rcdl:tf_reg_no2"]').send_keys(plateNumber[-4:])
+        browser.find_element_by_xpath('//*[@id="form_rcdl:j_idt32:CaptchaID"]').send_keys(captchaAnswer)
+        print(time.time() - start)
 
-    browser.find_element_by_xpath('//*[@id="form_rcdl:tf_reg_no1"]').send_keys(plateNumber[:-4])
-    browser.find_element_by_xpath('//*[@id="form_rcdl:tf_reg_no2"]').send_keys(plateNumber[-4:])
-    browser.find_element_by_xpath('//*[@id="form_rcdl:j_idt32:CaptchaID"]').send_keys(captchaAnswer)
-    print(time.time() - start)
-
-    # browser.find_element_by_xpath('//*[@id="form_rcdl:j_idt32:j_idt37"]').
-    img = browser.find_element_by_xpath('//*[@id="form_rcdl:j_idt32:j_idt37"]').get_attribute('src')
-    print(img)
-    print(time.time() - start)
-    urllib.request.urlretrieve(img, "captcha.png")
-    z = base64.b64encode(urllib.request.urlopen(img).read())
-    # print(sys. getsizeof(browser))
-    return jsonify({'msg': str(z[2:-1])}), 200
+        app.logger.info('test6')
+        # browser.find_element_by_xpath('//*[@id="form_rcdl:j_idt32:j_idt37"]').
+        img = browser.find_element_by_xpath('//*[@id="form_rcdl:j_idt32:j_idt37"]').get_attribute('src')
+        print(img)
+        print(time.time() - start)
+        urllib.request.urlretrieve(img, "captcha.png")
+        z = base64.b64encode(urllib.request.urlopen(img).read())
+        # print(sys. getsizeof(browser))
+        return jsonify({'msg': str(z[2:-1])}), 200
+    except Exception as e :
+        return jsonify({'msg': str(e)}), 200
 
 
 @app.route('/getdetails', methods=['POST'])
 def getdetails():
+    app.logger.info('test2')
     jsondata = request.get_data().decode("utf-8")
     jsondata = json.loads(jsondata)
 
     return jsonify({'msg': jsondata['code']}), 200
 @app.route('/')
 def About():
+    app.logger.info('test1')
     return jsonify({'About': 'STUFFFF'}), 200
 
 
@@ -118,4 +126,4 @@ if __name__ == '__main__':
 
     # print(time.time()-start)
     browser = webdriver.Chrome(options=option)
-    app.run()
+    app.run(debug=True)
