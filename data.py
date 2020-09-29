368eeb76a6a8c8c8a57e7a0683c4dec34be99032
Rishab Aggarwal <rishabaggarwal247@gmail.com>
2020-09-30 03:57:27 +0530

    api revival

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ class dataclass:
     chrome_options.add_argument("--headless")
     # chrome_options.add_argument('--no-proxy-server')
     # chrome_options.add_argument("--proxy-server='direct://'")
-    # chrome_options.add_argument("--proxy-bypass-list=*")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--remote-debugging-port=9222")
